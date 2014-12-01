3f37df021b92c9d5268d1198e65efa63f27305d9
Xiaoxu Wu <xiaoxushelly@gmail.com>
2014-12-01 15:50:42 -0800

    fix file writing bug
    do not start loading data until you hit "start data collection"

--- BodyBasics-WPF/MainWindow.xaml.cs
+++ BodyBasics-WPF/MainWindow.xaml.cs
@@ -134,6 +134,7 @@ namespace Microsoft.Samples.Kinect.BodyBasics
         /// 
         private string saveFilePath = "";
         private string saveFileName = "";
+        private bool data_streaming = false;
         private List<JointType> selectedJoints = new List<JointType>();
         public MainWindow()
         {
@@ -357,50 +358,53 @@ namespace Microsoft.Samples.Kinect.BodyBasics
 
                                 DepthSpacePoint depthSpacePoint = this.coordinateMapper.MapCameraPointToDepthSpace(position);
                                 jointPoints[jointType] = new Point(depthSpacePoint.X, depthSpacePoint.Y);
-                                if (jointType == JointType.FootRight && this.selectedJoints.Contains(JointType.FootRight))
+                                if (this.data_streaming)
                                 {
-                                    Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-                                    System.IO.File.AppendAllText(this.saveFilePath+"\\"+this.saveFileName+"FootRight.txt",
-                                        unixTimestamp+", "
-                                        +position.X + ", "
-                                        +position.Y + ", "
-                                        +position.Z + "\r\n");
-                                }
-                                if (jointType == JointType.FootLeft && this.selectedJoints.Contains(JointType.FootLeft))
-                                {
-                                    Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-                                    System.IO.File.AppendAllText(this.saveFilePath+"\\"+this.saveFileName+"FootLeft.txt",
-                                        unixTimestamp+", "
-                                        +position.X + ", "
-                                        +position.Y + ", "
-                                        +position.Z + "\r\n");
-                                }
-                                if (jointType == JointType.SpineBase && this.selectedJoints.Contains(JointType.SpineBase))
-                                {
-                                    Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-                                    System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "SpineBase.txt",
-                                        unixTimestamp + ", "
-                                        + position.X + ", "
-                                        + position.Y + ", "
-                                        + position.Z + "\r\n");
-                                }
-                                if (jointType == JointType.HipRight && this.selectedJoints.Contains(JointType.HipRight))
-                                {
-                                    Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-                                    System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "HipRight.txt",
-                                        unixTimestamp + ", "
-                                        + position.X + ", "
-                                        + position.Y + ", "
-                                        + position.Z + "\r\n");
-                                }
-                                if (jointType == JointType.HipLeft && this.selectedJoints.Contains(JointType.HipLeft))
-                                {
-                                    Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-                                    System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "HipLeft.txt",
-                                        unixTimestamp + ", "
-                                        + position.X + ", "
-                                        + position.Y + ", "
-                                        + position.Z + "\r\n");
+                                    if (jointType == JointType.FootRight && this.selectedJoints.Contains(JointType.FootRight))
+                                    {
+                                        Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+                                        System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "FootRight.txt",
+                                            unixTimestamp + ", "
+                                            + position.X + ", "
+                                            + position.Y + ", "
+                                            + position.Z + "\r\n");
+                                    }
+                                    if (jointType == JointType.FootLeft && this.selectedJoints.Contains(JointType.FootLeft))
+                                    {
+                                        Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+                                        System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "FootLeft.txt",
+                                            unixTimestamp + ", "
+                                            + position.X + ", "
+                                            + position.Y + ", "
+                                            + position.Z + "\r\n");
+                                    }
+                                    if (jointType == JointType.SpineBase && this.selectedJoints.Contains(JointType.SpineBase))
+                                    {
+                                        Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+                                        System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "SpineBase.txt",
+                                            unixTimestamp + ", "
+                                            + position.X + ", "
+                                            + position.Y + ", "
+                                            + position.Z + "\r\n");
+                                    }
+                                    if (jointType == JointType.HipRight && this.selectedJoints.Contains(JointType.HipRight))
+                                    {
+                                        Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+                                        System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "HipRight.txt",
+                                            unixTimestamp + ", "
+                                            + position.X + ", "
+                                            + position.Y + ", "
+                                            + position.Z + "\r\n");
+                                    }
+                                    if (jointType == JointType.HipLeft && this.selectedJoints.Contains(JointType.HipLeft))
+                                    {
+                                        Int64 unixTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+                                        System.IO.File.AppendAllText(this.saveFilePath + "\\" + this.saveFileName + "HipLeft.txt",
+                                            unixTimestamp + ", "
+                                            + position.X + ", "
+                                            + position.Y + ", "
+                                            + position.Z + "\r\n");
+                                    }
                                 }
                             }
                             //get the current datetime wrt 1970 in millisecond
@@ -582,6 +586,7 @@ namespace Microsoft.Samples.Kinect.BodyBasics
             { System.IO.File.WriteAllText(this.saveFilePath + "\\" + this.saveFileName + "HipRight.txt", header_line); }
             if (this.selectedJoints.Contains(JointType.HipRight))
             { System.IO.File.WriteAllText(this.saveFilePath + "\\" + this.saveFileName + "HipLeft.txt", header_line); }
+            this.data_streaming = true;
         }
         /// <summary>
         /// when clicking on the start data collection button, 
